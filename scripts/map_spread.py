0305375f80a00d095f0b5f48ae2167115cfe623a
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-04-21 20:29:53 -0700

    trying to make it "blink"

--- scripts/map_spread.py
+++ scripts/map_spread.py
@@ -17,7 +17,7 @@ plt.show()
 # Get the Laplacian
 L = nx.laplacian_matrix(G).toarray()
 # zero our L diagonal
-L[np.diag_indices_from(L)] = 0
+# L[np.diag_indices_from(L)] = 0
 # set an initial node state
 assert L.shape[0] == num_nodes
 x0 = np.zeros((L.shape[0], 1))
@@ -27,10 +27,10 @@ x0[2] = 1
 timesteps = 10
 x = x0
 for time in range(timesteps):
-    x = np.tanh(np.dot(L, x))
+    x = np.tanh(np.dot(1 / 10 * L, x))
     print(x)
     nx.draw(G, pos, node_size=100, node_color="black", with_labels=True)
-    active_nodes = np.where(x > 0)[0]
+    active_nodes = np.where(np.abs(x) > 0)[0]
     nx.draw(
         G, pos, nodelist=active_nodes, node_size=100, node_color="red", with_labels=True
     )
